2cdaa1bfc5bd388a928a4d917e9971a78052a6aa
jebeniani <aymenjebeniani05@gmail.com>
2023-07-17 17:08:25 +0100

    assertion

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,7 +1,4 @@
 void main(List<String> args) {
-  var score = 80;
-
-  assert(score >= 80, "Uncaught Error: Assertion failed: \"The score must be bigger or equal to 80\"");
-
-  print(score >= 80 ? "You Passed" : "");
+  var score = 85;
+  print(score >= 80 ? 'You Passed' : throw AssertionError("The score must be bigger or equal to 80"));
 }
