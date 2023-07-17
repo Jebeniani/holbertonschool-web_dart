eb36f1d6c12e213adae4aa4d717bc2cdcc7a1738
jebeniani <aymenjebeniani05@gmail.com>
2023-07-17 17:12:05 +0100

    assertion

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,4 +1,8 @@
 void main(List<String> args) {
-  var score = 85;
-  print(score >= 80 ? 'You Passed' : throw AssertionError("The score must be bigger or equal to 80"));
+  var score = 79;
+  try {
+    print(score >= 80 ? 'You Passed' : throw AssertionError("The score must be bigger or equal to 80"));
+  } catch (e) {
+    print(e);
+  }
 }
