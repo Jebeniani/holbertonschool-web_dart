426520bfc946324d3974a9fbee14bdb9417a4314
jebeniani <aymenjebeniani05@gmail.com>
2023-07-19 18:57:10 +0100

    basketball is my hobby

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -2,24 +2,14 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   int teamAPoints = 0;
   int teamBPoints = 0;
 
-  for (var shotType in teamA.keys) {
-    if (teamA.containsKey(shotType) && teamB.containsKey(shotType)) {
-      int teamAShots = teamA[shotType]!;
-      int teamBShots = teamB[shotType]!;
-      int points = 0;
-
-      if (shotType == 'Free throws') {
-        points = 1;
-      } else if (shotType == '2 pointer') {
-        points = 2;
-      } else if (shotType == '3 pointer') {
-        points = 3;
-      }
-
-      teamAPoints += teamAShots * points;
-      teamBPoints += teamBShots * points;
-    }
+  void calculatePoints(Map<String, int> team) {
+    teamAPoints += (team['Free throws'] ?? 0) +
+        (team['2 pointer'] ?? 0) * 2 +
+        (team['3 pointer'] ?? 0) * 3;
   }
+
+  calculatePoints(teamA);
+  calculatePoints(teamB);
 
   if (teamAPoints > teamBPoints) {
     return 1;
